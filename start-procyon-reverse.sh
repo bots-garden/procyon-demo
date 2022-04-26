a2bec4d17f99d5ec3948c47a39f5fd22d1bce02d
Philippe Charrière <ph.charriere@gmail.com>
2022-04-26 10:57:58 +0000

    📝 save

--- start-procyon-reverse.sh
+++ start-procyon-reverse.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
-cd procyon-reverse; PROXY_HTTP=8080 ./procyon-reverse
+cd procyon-reverse; 
+#PROXY_HTTP=8080 ./procyon-reverse
+PROXY_CRT="certs/procyon-reverse.local.crt" \
+PROXY_KEY="certs/procyon-reverse.local.key" \
+PROXY_HTTP=8080 \
+PROXY_HTTPS=4443 \
+./procyon-reverse
